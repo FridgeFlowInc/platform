fc25fde62c92dc7343b0dc117ead5b652aa90358
DRAKUTONT <grudnezlov@gmail.com>
2024-12-27 20:47:12 +0300

    product: add migrations, add views, add shemas,  update models

---
+++ backend/project/api/product/shemas.py
@@ -0,0 +1,42 @@
+import datetime
+import uuid
+
+from ninja import Schema
+
+
+class ProductResponse(Schema):
+    id: uuid.UUID
+    name: str
+    category: str
+    manufacturer: str
+    quantity: float
+    unit: str
+    nutritional_value: float
+    total_net_weight: float
+    total_gross_weight: float
+    manufacture_date: datetime.date
+    expiration_date: datetime.date
+    notes: str
+    allergens: str
+
+
+class ProductCreate(Schema):
+    name: str
+    category: str
+    manufacturer: str
+    quantity: float
+    unit: str
+    nutritional_value: float
+    total_net_weight: float
+    total_gross_weight: float
+    manufacture_date: datetime.date
+    expiration_date: datetime.date
+    notes: str
+    allergens: str
+
+
+class ProductLogResponse(Schema):
+    product: uuid.UUID
+    action_type: str
+    description: str
+    timestamp: datetime.datetime
--- backend/project/api/router.py
+++ backend/project/api/router.py
@@ -1,11 +1,15 @@
 from ninja import NinjaAPI
 
+import project.api.auth
 import project.api.health.views
+import project.api.product.views
 
 api = NinjaAPI(
     title="FridgeFlow API",
     version="0.0.1",
     description="API docs for FridgeFlow project",
+    auth=project.api.auth.BearerAuth(),
 )
 
 api.add_router("/health/", project.api.health.views.router)
+api.add_router("/product/", project.api.product.views.router)
