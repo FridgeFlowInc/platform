1e4dd82f8a256f28b818328bc6b1396605be534a
ITQ <itq.dev@ya.ru>
2024-12-27 19:22:14 +0300

    chore: added ninja to app list

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # Third-party apps
+    "ninja",
     # Internal apps
     "project.api.health",
     "project.api.product",
