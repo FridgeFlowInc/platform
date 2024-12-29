a1544442e262969bb0376b3ba1c9c508f9a1b868
ITQ <itq.dev@ya.ru>
2024-12-29 16:00:44 +0300

    chore: small improvement in check.sh script

--- backend/check.sh
+++ backend/check.sh
@@ -3,8 +3,8 @@
 GREEN='\033[1;32m'
 NC='\033[0m'
 
-uvx ruff check . --fix
 uvx ruff format .
+uvx ruff check . --fix
 printf "${GREEN}Linters/formatters runned${NC}\n"
 
 uv run python manage.py makemigrations --check
