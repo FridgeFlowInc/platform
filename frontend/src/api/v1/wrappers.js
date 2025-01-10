f86dc8d47e7d75aec46e8749ae1358741045186b
ITQ <itq.dev@ya.ru>
2025-01-10 21:49:54 +0300

    chore: temporary workout
    i promise

--- frontend/src/api/v1/wrappers.js
+++ frontend/src/api/v1/wrappers.js
@@ -3,12 +3,14 @@ import axios from 'axios';
 import { useStore } from '@/store/zustand';
 
 const axiosWithoutAuth = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  // baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  baseURL: `http://176.108.253.40:8001/api/v1`,
   timeout: 10000,
 });
 
 const axiosWithAuth = axios.create({
-  baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  // baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
+  baseURL: `http://176.108.253.40:8001/api/v1`,
   timeout: 10000,
 });
 
