faa945ca300858baa480fb291cbd5307380bbbd2
ITQ <itq.dev@ya.ru>
2025-01-18 18:52:04 +0300

    feat: added analytics and removed unused files

---
+++ frontend/src/features/analytics/index.tsx
@@ -0,0 +1,22 @@
+import { Header } from '@/components/layout/header'
+import { Main } from '@/components/layout/main'
+import { ProfileDropdown } from '@/components/profile-dropdown'
+import { ThemeSwitch } from '@/components/theme-switch'
+import { analyticsGet } from '@/api/v1/analytics'
+
+export default function Analytics() {
+  analyticsGet()
+  return (
+    <>
+      <Header>
+        <div className='ml-auto flex items-center space-x-4'>
+          <ThemeSwitch />
+          <ProfileDropdown />
+        </div>
+      </Header>
+
+      <Main>
+      </Main>
+    </>
+  )
+}
