5f2efd60f0cd670464ab5c026dc52dc4ed64e922
ITQ <itq.dev@ya.ru>
2025-01-22 18:25:23 +0300

    fix(frontend): fixed analytics API interactor

--- frontend/src/api/v1/analytics/index.ts
+++ frontend/src/api/v1/analytics/index.ts
@@ -1,9 +1,9 @@
 import { axiosWithAuth } from '@/api/v1/wrappers'
 
-export async function analyticsGet() {
+export async function analyticsGet(date_after: string, date_before: string) {
   const response = await axiosWithAuth({
-    method: 'post',
-    url: `/product/analytics`,
+    method: 'get',
+    url: `/product/analytics?date_after=${date_after}&date_before=${date_before}`,
   })
   return response.data
 }
