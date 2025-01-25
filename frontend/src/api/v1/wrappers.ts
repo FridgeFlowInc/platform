db0a3735fea90191f0dc75582172cf4dc388d538
ITQ <itq.dev@ya.ru>
2025-01-25 16:32:18 +0300

    chore: removed temporary workout

--- frontend/src/api/v1/wrappers.ts
+++ frontend/src/api/v1/wrappers.ts
@@ -3,13 +3,11 @@ import { useAuthStore } from '@/stores/authStore'
 
 export const axiosWithoutAuth = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
-  // baseURL: `http://176.108.253.40:8001/api/v1`,
   timeout: 10000,
 })
 
 export const axiosWithAuth = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api/v1`,
-  // baseURL: `http://176.108.253.40:8001/api/v1`,
   timeout: 10000,
 })
 
