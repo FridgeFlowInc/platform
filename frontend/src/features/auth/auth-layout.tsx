ffb08d53f62c7f0f2dbe7d531cf7ac76cbc84e1a
ITQ <itq.dev@ya.ru>
2025-01-25 16:32:48 +0300

    chore(frontend): small ui improvements

--- frontend/src/features/auth/auth-layout.tsx
+++ frontend/src/features/auth/auth-layout.tsx
@@ -11,7 +11,7 @@ export default function AuthLayout({ children }: Props) {
       <div className='absolute top-4 right-4'>
         <ThemeSwitch />
       </div>
-      <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0'>
+      <div className='container grid h-svh flex-col items-center justify-center bg-primary-foreground lg:max-w-none lg:px-0 pl-1 pr-1'>
         <div className='mx-auto flex w-full flex-col justify-center space-y-2 sm:w-[480px] lg:p-8'>
           <div className='mb-4 flex items-center justify-center'>
             <IconFridge className='mr-2 h-6 w-6' />
