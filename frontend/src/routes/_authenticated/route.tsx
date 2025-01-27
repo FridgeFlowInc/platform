47a777187b78ab947be879245d7871d4f8282de4
ITQ <itq.dev@ya.ru>
2025-01-27 20:40:29 +0300

    chore(frontend): removed search and unused deps

--- frontend/src/routes/_authenticated/route.tsx
+++ frontend/src/routes/_authenticated/route.tsx
@@ -4,7 +4,6 @@ import { createFileRoute, Outlet, useNavigate } from '@tanstack/react-router'
 import { toast } from 'sonner'
 import { useAuthStore } from '@/stores/authStore'
 import { cn } from '@/lib/utils'
-import { SearchProvider } from '@/context/search-context'
 import { SidebarProvider } from '@/components/ui/sidebar'
 import { AppSidebar } from '@/components/layout/app-sidebar'
 import SkipToMain from '@/components/skip-to-main'
@@ -28,25 +27,23 @@ function RouteComponent() {
   }, [checkedAuthentication, isAuthenticated])
 
   return (
-    <SearchProvider>
-      <SidebarProvider defaultOpen={defaultOpen}>
-        <SkipToMain />
-        <AppSidebar />
-        <div
-          id='content'
-          className={cn(
-            'max-w-full w-full ml-auto',
-            'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
-            'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
-            'transition-[width] ease-linear duration-200',
-            'h-svh flex flex-col',
-            'group-data-[scroll-locked=1]/body:h-full',
-            'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
-          )}
-        >
-          {isAuthenticated ? <Outlet /> : null}
-        </div>
-      </SidebarProvider>
-    </SearchProvider>
+    <SidebarProvider defaultOpen={defaultOpen}>
+      <SkipToMain />
+      <AppSidebar />
+      <div
+        id='content'
+        className={cn(
+          'max-w-full w-full ml-auto',
+          'peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-1rem)]',
+          'peer-data-[state=expanded]:w-[calc(100%-var(--sidebar-width))]',
+          'transition-[width] ease-linear duration-200',
+          'h-svh flex flex-col',
+          'group-data-[scroll-locked=1]/body:h-full',
+          'group-data-[scroll-locked=1]/body:has-[main.fixed-main]:h-svh'
+        )}
+      >
+        {isAuthenticated ? <Outlet /> : null}
+      </div>
+    </SidebarProvider>
   )
 }
